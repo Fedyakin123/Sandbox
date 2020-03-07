99394a49f999f0984bca51289945ba4b19f8ece1
Roman <Skabaz@yandex.ru>
2020-03-08 00:08:29 +0300

    User model rady

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('username', 'email', 'age', 'staff_code', 'is_staff')
+
+admin.site.register(CustomUser, CustomUserAdmin)
