0bdaeca783287933f1b51838c7ffe4ef36e95c80
Roman <Skabaz@yandex.ru>
2020-03-08 23:59:25 +0300

    branch

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import (
+    CustomUserCreationForm,
+    CustomUserChangeForm,
+)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('username', 'country', 'gender')
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    gender = models.CharField(max_length=2, null = True, blank = True)
+    country = models.CharField(max_length=50, null = True, blank = True)
+
